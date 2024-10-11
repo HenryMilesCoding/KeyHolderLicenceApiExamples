cb64247fbd438b83da3cde868a7480d0f78dc556
HenryMilesCoding <michael-herzau@gmx.de>
2024-10-12 00:42:21 +0200

    Modelausgabe erweitert

--- Desktop/HenryMilesLicenceOnlineWpf/MainWindow.xaml.cs
+++ Desktop/HenryMilesLicenceOnlineWpf/MainWindow.xaml.cs
@@ -32,51 +32,66 @@ namespace HenryMilesLicenceOnlineWpf
     public partial class MainWindow : Window
     {
         public IConfiguration optionalSecrets;
-        public Configuration config;
-        public ApiCreatesService HenryCreate { get; private set; }
-        public ApiCheckService HenryCheck { get; private set; }
-        public ApiReadsService HenryReader { get; private set; }
-        public CreateaccountModel actualUser { get; private set; }
+        public Configuration? config;
+        public ApiCreatesService? HenryCreate { get; private set; }
+        public ApiCheckService? HenryCheck { get; private set; }
+        public ApiReadsService? HenryReader { get; private set; }
+        public CreateaccountModel? ActualUser { get; private set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            HenryCreate = new ApiCreatesService();
-            HenryCreate.fireOn = SystemEnvironmentDbEnum.Tst;
-            HenryCheck = new ApiCheckService();
-            HenryCheck.fireOn = HenryCreate.fireOn;
-            HenryReader = new ApiReadsService();
-            HenryReader.fireOn = HenryCreate.fireOn;
-            actualUser = new CreateaccountModel();
 
-            var builder = new ConfigurationBuilder()
-            .AddUserSecrets<MainWindow>();
+            try
+            {
+                config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                HenryCreate = new ApiCreatesService();
+                HenryCreate.fireOn = SystemEnvironmentDbEnum.Tst;
+                HenryCheck = new ApiCheckService();
+                HenryCheck.fireOn = HenryCreate.fireOn;
+                HenryReader = new ApiReadsService();
+                HenryReader.fireOn = HenryCreate.fireOn;
+                ActualUser = new CreateaccountModel();
 
-            optionalSecrets = builder.Build();
+                var builder = new ConfigurationBuilder()
+                .AddUserSecrets<MainWindow>();
 
-            FillKeyAndLicence();
+                optionalSecrets = builder.Build();
+
+                FillKeyAndLicence();
+            }
+            catch (Exception ex)
+            {
+                FillDisplay(ex.Message);
+            }
         }
 
         private void FillKeyAndLicence()
         {
-            if (string.IsNullOrEmpty(config.AppSettings.Settings[ConfigKey.NewAccountModel].Value))
+            try
             {
-                FillDisplay("You must first create an account before this step works.");
-                return;
+                if (string.IsNullOrEmpty(config.AppSettings.Settings[ConfigKey.NewAccountModel].Value))
+                {
+                    FillDisplay("You must first create an account before this step works.");
+                    return;
+                }
+
+                ActualUser = JsonConvert.DeserializeObject<CreateaccountModel>(config.AppSettings.Settings[ConfigKey.NewAccountModel].Value);
+
+                if (ActualUser != null)
+                {
+                    PublicKeyTextBox.Text = ActualUser.Backdata.InfosToYourNewAccount.YourNewPublicKey;
+                }
+
+                string lastLicence = config.AppSettings.Settings[ConfigKey.LicenceKey].Value;
+                if (lastLicence != null)
+                {
+                    LicenceTextBox.Text = lastLicence;
+                }
             }
-
-            actualUser = JsonConvert.DeserializeObject<CreateaccountModel>(config.AppSettings.Settings[ConfigKey.NewAccountModel].Value);
-
-            if (actualUser != null)
+            catch (Exception ex)
             {
-                PublicKeyTextBox.Text = actualUser.Backdata.InfosToYourNewAccount.YourNewPublicKey;
-            }
-
-            string lastLicence = config.AppSettings.Settings[ConfigKey.LicenceKey].Value;
-            if (lastLicence != null)
-            {
-                LicenceTextBox.Text = lastLicence;
+                FillDisplay(ex.Message);
             }
         }
 
@@ -108,11 +123,11 @@ namespace HenryMilesLicenceOnlineWpf
         {
             try
             {
-                string privateKey = actualUser.Backdata.InfosToYourNewAccount.YourNewPrivateKey;
-                string secret = actualUser.Backdata.InfosToYourNewAccount.YourSecret;
+                string privateKey = ActualUser.Backdata.InfosToYourNewAccount.YourNewPrivateKey;
+                string secret = ActualUser.Backdata.InfosToYourNewAccount.YourSecret;
                 string licence = LicenceTextBox.Text;
 
-                if (actualUser == null)
+                if (ActualUser == null)
                 {
                     FillDisplay("You must first create an account for it to work!");
                     return;
@@ -144,84 +159,133 @@ namespace HenryMilesLicenceOnlineWpf
 
         private void FillDisplay(object value)
         {
-            if (value == null || ApiResponseTextBox == null)
+            try
             {
-                return;
+                if (value == null || ApiResponseTextBox == null)
+                {
+                    return;
+                }
+
+                string bringUp = string.Empty;
+
+                if (value is object)
+                {
+                    bringUp = JsonConvert.SerializeObject(value, Formatting.Indented);
+                }
+                else
+                {
+                    bringUp = value.ToString();
+                }
+
+                ApiResponseTextBox.Text = bringUp;
             }
-
-            string bringUp = string.Empty;
-
-            if (value is object)
+            catch (Exception ex)
             {
-                bringUp = JsonConvert.SerializeObject(value, Formatting.Indented);
+                FillDisplay(ex.Message);
             }
-            else
-            {
-                bringUp = value.ToString();
-            }
-
-            ApiResponseTextBox.Text = bringUp;
         }
 
         private void LicenceModelButton_Click(object sender, RoutedEventArgs e)
         {
-            GetKeylistRequestModel accountModel = HenryCreate.CreateLicenceTemplate("<SET_PRIVATE_KEY>", "<SET_SECRET>");
-            // Attention: Enum values cannot be displayed correctly in the text window. The text values of enums are then always
-            FillDisplay(accountModel);
+            try
+            {
+                GetKeylistRequestModel accountModel = HenryCreate.CreateLicenceTemplate("<SET_PRIVATE_KEY>", "<SET_SECRET>");
+                // Attention: Enum values cannot be displayed correctly in the text window. The text values of enums are then always
+                FillDisplay(accountModel);
+            }
+            catch (Exception ex)
+            {
+                FillDisplay(ex.Message);
+            }
         }
 
         private void NewAccountButton_Click(object sender, RoutedEventArgs e)
         {
-            CreateaccountModel newAccount = HenryCreate.CreateNewAccount();
-            string bringUp = JsonConvert.SerializeObject(newAccount, Formatting.Indented);
-            FillDisplay(newAccount);
-
-            if (newAccount != null)
+            try
             {
-                ConfigHelper.UpdateValue(config, ConfigKey.NewAccountModel, bringUp, INeedSection.appSettings);
-                FillKeyAndLicence();
+                CreateaccountModel newAccount = HenryCreate.CreateNewAccount();
+                string bringUp = JsonConvert.SerializeObject(newAccount, Formatting.Indented);
+                FillDisplay(newAccount);
+
+                if (newAccount != null)
+                {
+                    ConfigHelper.UpdateValue(config, ConfigKey.NewAccountModel, bringUp, INeedSection.appSettings);
+                    FillKeyAndLicence();
+                }
+            }
+            catch (Exception ex)
+            {
+                FillDisplay(ex.Message);
             }
         }
 
         private void GetLicenceButton_Click(object sender, RoutedEventArgs e)
         {
-            if (actualUser == null)
+            try
             {
-                FillDisplay("You must first create an account before this step works.");
-                return;
+                if (ActualUser == null)
+                {
+                    FillDisplay("You must first create an account before this step works.");
+                    return;
+                }
+
+                GetKeylistRequestModel requestForList = new GetKeylistRequestModel();
+                requestForList.secret = ActualUser.Backdata.InfosToYourNewAccount.YourSecret;
+                requestForList.userKey = ActualUser.Backdata.InfosToYourNewAccount.YourNewPrivateKey;
+                GetkeylistModel resultForList = HenryReader.GetKeylist(requestForList, false);
+                // Attention: Enum values cannot be displayed correctly in the text window. The text values of enums are then always
+
+                FillDisplay(resultForList);
             }
-
-            GetKeylistRequestModel requestForList = new GetKeylistRequestModel();
-            requestForList.secret = actualUser.Backdata.InfosToYourNewAccount.YourSecret;
-            requestForList.userKey = actualUser.Backdata.InfosToYourNewAccount.YourNewPrivateKey;
-            GetkeylistModel resultForList = HenryReader.GetKeylist(requestForList, false);
-            // Attention: Enum values cannot be displayed correctly in the text window. The text values of enums are then always
-
-            FillDisplay(resultForList);
+            catch (Exception ex)
+            {
+                FillDisplay(ex.Message);
+            }
         }
 
         private void NewLicenceButton_Click(object sender, RoutedEventArgs e)
         {
-            if (actualUser == null)
+            try
             {
-                FillDisplay("You must first create an account before this step works.");
-                return;
+                if (ActualUser == null)
+                {
+                    FillDisplay("You must first create an account before this step works.");
+                    return;
+                }
+
+                string userKey = ActualUser.Backdata.InfosToYourNewAccount.YourNewPrivateKey;
+                string secret = ActualUser.Backdata.InfosToYourNewAccount.YourSecret;
+
+                GetKeylistRequestModel templateToCreate = HenryCreate.CreateLicenceTemplate(userKey, secret);
+                CreatelicenceModel result = HenryCreate.CreateNewLicence(templateToCreate);
+
+                if (result != null)
+                {
+                    FillDisplay(result);
+                    ConfigHelper.UpdateValue(config, ConfigKey.LicenceKey, result.Backdata.NewKey, INeedSection.appSettings);
+                    FillKeyAndLicence();
+                }
             }
-
-            string userKey = actualUser.Backdata.InfosToYourNewAccount.YourNewPrivateKey;
-            string secret = actualUser.Backdata.InfosToYourNewAccount.YourSecret;
-
-            GetKeylistRequestModel templateToCreate = HenryCreate.CreateLicenceTemplate(userKey, secret);
-            CreatelicenceModel result = HenryCreate.CreateNewLicence(templateToCreate);
-
-            if (result != null)
+            catch (Exception ex)
             {
-                FillDisplay(result);
-                ConfigHelper.UpdateValue(config, ConfigKey.LicenceKey, result.Backdata.NewKey, INeedSection.appSettings);
-                FillKeyAndLicence();
+                FillDisplay(ex.Message);
             }
         }
 
-        // Weitere API-Endpunkt-Methoden...
+        private void KeyListModelButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                GetKeylistRequestModel requestForList = new GetKeylistRequestModel();
+                requestForList.secret = "<HERE_YOUR_SECRET>";
+                requestForList.userKey = "<HERE_YOUR_PRIVATE_KEY>";
+
+                FillDisplay(requestForList);
+            }
+            catch (Exception ex)
+            {
+                FillDisplay(ex.Message);
+            }
+        }
     }
 }
